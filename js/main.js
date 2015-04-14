4ee8763e1968617d7489520dba2a9c698eaa7ca3
Guy Willis <guy.willis90@gmail.com>
2015-04-14 20:06:24 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+/*
+$(function(){
+	$(".extBodyBar").on('click', function() {
+		if ($(this).children("img").attr("class") == "iconSwap") {
+			$(this).children("img").attr('src','/u/Common/iconOpen.png');
+		}
+		else {
+			$(this).children("img").attr('src','/u/Common/iconNext.png');
+		}
+		$(this).children("img").toggleClass("on");
+	});
+}); 
+*/
+/*
+$(document).ready(function() {
+	$("[id$=txtPassword]").keypress(function(event){
+	    if(event.keyCode == 13){
+	    	event.preventDefault();
+	        $("[id$=btnLogOn]").click();
+	    }
+	});
+});
+*/
